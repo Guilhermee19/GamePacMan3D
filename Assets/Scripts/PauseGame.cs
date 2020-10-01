34eb87da0e4b6c9b06ab684a0536e268d9faecf9
Guilherme <guilherme.santana1998@gmail.com>
2020-10-01 03:53:53 -0300

    coletar item, pontuação na tela, camera primeira pessoa

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PauseGame : MonoBehaviour
 {
     public GameObject textPause;
+    float cameraPitch = 0.0f;
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape)){
             if(Time.timeScale == 1){
                 textPause.SetActive(true);
+                
                 Time.timeScale = 0;
             }
             else{
                 textPause.SetActive(false);
+                //Cursor.lockState = CursorLockMode.Locked;
                 Time.timeScale = 1;
             }
         }
---
+++ Assets/Scripts/collectItems.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collectItems : MonoBehaviour
+{
+    public Text punctuation;
+    private int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        punctuation.text = "Pontos: "+ points;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if( other.tag == "collectable_items"){
+            other.gameObject.SetActive(false);
+            points = points + 1;
+        }
+        
+    }
+}
